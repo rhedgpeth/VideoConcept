2c3490307fe2cf5410bbbaf3ba2946fdf109ff4e
Will Smith <lol.tihan@gmail.com>
2017-02-15 17:35:37 -0800

    Added Forms

---
+++ VideoConcept/VideoConceptPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+using CrossCamera.Core;
+
+namespace VideoConcept
+{
+	public partial class VideoConceptPage : ContentPage
+	{
+		public VideoConceptPage()
+		{
+			InitializeComponent();
+
+			this.CaptureVideoButton.Clicked += async (sender, e) =>
+			{
+				try
+				{
+					var vf = Camera.Current.OpenVideoFile(Camera.Current.DefaultVideoSaveDirectory + "/test.mov");
+					Camera.Current.DeleteVideoFile(vf);
+
+					vf = Camera.Current.OpenVideoFile(Camera.Current.DefaultVideoSaveDirectory + "/test.mp4");
+					Camera.Current.DeleteVideoFile(vf);
+				}
+				catch
+				{
+				}
+
+				var videoFile = await Camera.Current.TakeVideoAsync("test.mp4");
+
+				if (videoFile != null)
+				{
+					videoFile.Dispose();
+				}
+			};
+		}
+	}
+}
